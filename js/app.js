b6395846e1b01211dab24066c79d66a9bd709f83
11zouzou11 <josephchahine@live.com>
2022-02-07 09:16:52 +0000

    adding the emailjs

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
-if (//scroll position reached) {
-  $('nav').addClass(navbar-fixed-top)
-}
+emailjs.sendForm('service_p95q5eq', 'template_ap3sstw', '#form',
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
