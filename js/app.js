2fc1c5c77b9b4b0186a8c5b54c5a3b14b37d28f0
11zouzou11 <josephchahine@live.com>
2022-02-23 22:21:20 +0000

    added the info at the footer and removed the js folder

--- js/app.js
+++
@@ -1,6 +0,0 @@
-emailjs.sendForm('service_p95q5eq', 'template_ap3sstw', '#form',
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
-    });
